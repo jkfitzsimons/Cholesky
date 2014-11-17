bc229b4e247998d28f6cd60207e279787655660d
Jack Fitzsimons <jack.fitzsimons@eng.ox.ac.uk>
2014-11-17 11:43:04 +0000

    Updated to be a plug and play
    Alternative to jitchol as seen in GPy and pyGPs. Applies the same sanity checks and returns equivalent errors / warnings.

--- cholesky.py
+++ cholesky.py
@@ -29,76 +29,86 @@ Platform: Written for MacOSX with Accelerate Framework (cBlas)
 import scipy.weave as wv
 import numpy as np
 
-def safe_chol(Y):
+def safe_chol(Y,maxtries=20):
     # The C source for the cholesky decomposition with cBLAS
     c_source = '''
+        double in = 0;
         X[0] = sqrt(X[0]);
-
-        // Used to
+        
+        // Used as accumulator
         double s;
-
-
+        double count = 0;
+        
         long d_size = &X[1] - &X[0];
-
+        
         // Pointers will be used to reference start of columns
         // for matrix arithmatic
         double* X_local_1;
         double* X_local_2;
         int len;
-
+        
         for(int k = 1; k < n; k++) {
-            // Evaluate the off diagonals
-            for(int i = 0; i < k; i++) {
-                s = 0;
-                X_local_1 = X+(i*n*d_size);
-                X_local_2 = X+(k*n*d_size);
-                len = i;
-                s = cblas_ddot(len, X_local_1, 1, X_local_2, 1);
-
-                X[(k*n)+i] = (X[(k*n)+i] - s)/X[(i*n)+i];
-            }
-
-            // Evaluate the diagonals
-            int len = k;
-            X_local_1 = X+(k*n*d_size);
-            s = cblas_ddot(len, X_local_1, 1, X_local_1, 1);
-            if(s >= X[(k*n)+k] - 0.00005){
-                // Force matrix to be PSD
-                X[(k*n)+k] = 0.00005;
-            }
-            else{
-                // This sqrt() will fail if not PSD
-                X[(k*n)+k] = sqrt(X[(k*n)+k]-s);
-            }
-
+        // Evaluate the off diagonals
+        for(int i = 0; i < k; i++) {
+        s = 0;
+        X_local_1 = X+(i*n*d_size);
+        X_local_2 = X+(k*n*d_size);
+        len = i;
+        s = cblas_ddot(len, X_local_1, 1, X_local_2, 1);
+        
+        X[(k*n)+i] = (X[(k*n)+i] - s)/X[(i*n)+i];
         }
-
+        
+        // Evaluate the diagonals
+        int len = k;
+        X_local_1 = X+(k*n*d_size);
+        s = cblas_ddot(len, X_local_1, 1, X_local_1, 1);
+        if(s >= X[(k*n)+k] - noise){
+        // Force matrix to be PSD
+        X[(k*n)+k] = noise;
+        in++;
+        }
+        else{
+        // This sqrt() will fail if not PSD
+        X[(k*n)+k] = sqrt(X[(k*n)+k]-s);
+        }
+        
+        }
         // Zero out upper triangle of matrix
         for(int k = 0; k < n-1; k++) {
-            for(int i = k+1; i < n; i++) {
-                X[(k*n)+i] = 0;
-            }
+        for(int i = k+1; i < n; i++) {
+        X[(k*n)+i] = 0;
         }
+        }
+        
+        // Can uncomment if you desire feedback on the amount of observations with noise added
+        //std::cout << "Added noise to " << in*100/n << "% of the observations" << std::endl;
+        
+        '''
+    X = np.array(A, copy=True, dtype=np.float64)
 
-    '''
-
-    # The rest of the code merely wraps the c code in scipy weave and calls the function
-
-    # Make a copy of X so that we don't make changes to it
-    X = np.copy(Y)
-
-    # Find the dimensionality of Y
-    n = X.shape[0]
-
-    # Confirm that we are dealing with a square matrix
-    if n != X.shape[1]:
-        print('Matrix is not square, invalid arguments.')
-        return None
-
-    # If you are using a Windows or Linux machine please edit appropriate header file and framework arguments to link cBLAS
-    wv.inline(c_source,['X', 'n'], headers = ["<math.h>", "<Accelerate/Accelerate.h>"], extra_link_args = ["-framework Accelerate"])
-
-    return X
+    try:
+        L = np.linalg.cholesky(X)
+        return L
+    except:
+        diagX = np.diag(X)
+        # Find the dimensionality of Y
+        n = X.shape[0]
+        if np.any(np.tril(X) != np.tril(X.T)):
+            raise np.linalg.LinAlgError, "kernel matrix not symetric"
+        if np.any(diagX <= 0.):
+            raise np.linalg.LinAlgError, "kernel matrix not positive definite: non-positive diagonal elements"
+        noise = 0.00005
+        while maxtries > 0 and np.isfinite(noise):
+            print 'Warning: adding allowed noise of {:.10e}'.format(noise)
+            # If you are using a Windows or Linux machine please edit appropriate header file and framework arguments to link cBLAS
+            wv.inline(c_source,['X', 'n', 'noise'], headers = ["<math.h>", "<Accelerate/Accelerate.h>", "<iostream>"], extra_link_args = ["-framework Accelerate"])
+            if not np.any(np.isinf(X) + np.isnan(X)):
+                return X
+            X = np.array(A, copy=True, dtype=np.float64)
+            noise *= 10
+            maxtries -= 1
+        raise np.linalg.LinAlgError, "kernel matrix not positive definite, even with noise."
 
 
 # This code simply allows you to test the functionality of safe_chol()
